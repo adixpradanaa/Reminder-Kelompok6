35c5037a8ce260e3a88d5d1ef49a5cdfd0733af3
Adi Pradana <officemail.adipradana@gmail.com>
2021-08-15 16:05:24 +0700

    Initial Release

---
+++ app/src/main/java/com/example/reminder_kel6/Data.java
@@ -0,0 +1,32 @@
+package com.example.reminder_kel6;
+
+public class Data {
+
+    int id;
+    private String title;
+    private String date;
+    private String time;
+
+    Data(int id, String title, String date, String time) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    String getDate() {
+        return date;
+    }
+
+    String getTime() {
+        return time;
+    }
+}
